af7f689c12f40138db80f850f61f8bf4d09f1b5f
sads4ndwich <caiocamargo007@gmail.com>
2024-09-23 22:41:58 -0300

    feat: allocate a block of memory with a header info

--- includes/sadloc.h
+++ includes/sadloc.h
@@ -3,7 +3,25 @@
 #ifndef _SADLOCH_
 #define _SADLOCH_
 
-/* Alloc a block of memory of given `size` */
+typedef char ALIGN[16];
+
+union header {
+    struct {
+        size_t size;        // Block of memory's size
+        unsigned is_free;   // Flag if block of memory is free
+        union header *next; // Pointer to next block of memory
+    } s;
+
+    ALIGN stup;
+};
+
+// Block of memory's header info
+typedef union header header_t;
+
+// Alloc a block of memory of given `size`
 void *sadloc(size_t size);
+
+// Return the first free block of memory of the given size (first-fit)
+header_t *get_free_block(size_t size);
 
 #endif
--- src/sadloc.c
+++ src/sadloc.c
@@ -1,5 +1,6 @@
 #include <unistd.h>
 #include <stdlib.h>
+#include <pthread.h>
 
 #include "sadloc.h"
 
@@ -14,11 +15,58 @@
     On failure, it returns `(void *) -1`.
 */
 
+header_t *head, *tail;
+
+pthread_mutex_t sadloc_mutex;
+
 void *sadloc(size_t size) {
+    size_t total_size;
+    header_t *header;
     void *block;
 
-    block = sbrk(size);
-    if (block == (void *) -1) return NULL;
+    if (!size) {
+        return NULL;
+    }
 
-    return block;
+    // Prevent multiple threads to allocate at same time
+    pthread_mutex_lock(&sadloc_mutex);
+
+    header = get_free_block(size);
+    if (header) {
+        header->s.is_free = 0;
+        pthread_mutex_unlock(&sadloc_mutex);
+        return (void *) (header + 1);
+    }
+
+    total_size = sizeof(header_t) + size;
+
+    block = sbrk(total_size);
+    if (block == (void *) -1) {
+        pthread_mutex_unlock(&sadloc_mutex);
+        return NULL;
+    }
+
+    header = block;
+    header->s.size = size;
+    header->s.is_free = 0;
+    header->s.next = NULL;
+
+    if (!head) head = header;
+    if (tail) tail->s.next = header;
+
+    tail = header;
+
+    pthread_mutex_unlock(&sadloc_mutex);
+    return (void *) (header + 1);
+}
+
+header_t *get_free_block(size_t size) {
+    header_t *curr = head;
+
+    while (curr) {
+        if (curr->s.is_free && curr->s.size >= size) return curr;
+        curr = curr->s.next;
+    }
+
+    return NULL;
 }
